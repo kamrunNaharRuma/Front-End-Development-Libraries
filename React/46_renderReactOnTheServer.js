f01a27dac3fa1519a037735552faeaa8df7c2c5c
kamrunnaharruma <knahar12bd@gmail.com>
2021-09-30 18:52:56 +0600

    React

---
+++ React/46_renderReactOnTheServer.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div/>
+    }
+};
+ReactDOMServer.renderToString(<App/>)
+
+// Change code below this line
