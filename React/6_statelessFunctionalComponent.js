7ab4bcde8b5f1031acfe66cefd9234831545c507
kamrunnaharruma <knahar12bd@gmail.com>
2021-09-30 18:51:48 +0600

    React

---
+++ React/6_statelessFunctionalComponent.js
@@ -0,0 +1,9 @@
+const MyComponent = function() {
+    // Change code below this line
+    return (
+        <div>{ 'some string or tag'} </div>
+    );
+
+
+    // Change code above this line
+}
---
+++ React/7_creating ComponentWithReactClass.js
@@ -0,0 +1,18 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        // Change code below this line
+        return (
+            <div>
+                <h1>
+                    Hello react!
+                </h1>
+            </div>
+        );
+
+
+        // Change code above this line
+    }
+};
